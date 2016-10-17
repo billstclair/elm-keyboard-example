c536bce38b864d9076a93651fd849d3c4d8f7f2b
Bill St. Clair <billstclair@gmail.com>
2016-10-17 08:01:16 -0400

    Add Keyboard.downs and Keyboard.ups

--- keyboard-test.elm
+++ keyboard-test.elm
@@ -16,6 +16,7 @@ import Html exposing (Html, Attribute, div, text, h2, p, span, a)
 import Html.Attributes exposing (style, href)
 import Html.App as Html
 import Debug exposing (log)
+import Platform.Sub exposing (batch)
 
 main =
   Html.program
@@ -26,13 +27,24 @@ main =
     }
 
 type alias Model =
-  Int
+  { press: Int
+  , down: Int
+  , up: Int
+  }
+
+qm : Int
+qm = Char.toCode '?'
 
 model : Model
-model = 0
+model = { press = qm
+        , down = qm
+        , up = qm
+        }
 
-type Msg =
-  PressKey Int
+type Msg
+  = KeyPressed Int
+  | KeyDown Int
+  | KeyUp Int
 
 init : (Model, Cmd Msg)
 init = (model, Cmd.none)
@@ -40,12 +52,19 @@ init = (model, Cmd.none)
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-      PressKey key ->
-        (log "KeyCode:" key, Cmd.none)
+      KeyPressed key ->
+        ({ model | press = log "KeyPressed:" key }, Cmd.none)
+      KeyDown key ->
+        ({ model | down = log "KeyDown:" key }, Cmd.none)
+      KeyUp key ->
+        ({ model | up = log "KeyUp:" key }, Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Keyboard.presses (PressKey)
+  batch [ Keyboard.presses KeyPressed
+        , Keyboard.downs KeyDown
+        , Keyboard.ups KeyUp
+        ]
 
 bold: Attribute a
 bold =
@@ -55,20 +74,30 @@ br : Html a
 br =
   Html.br [] []
 
+displayCode : Int -> String -> Html a
+displayCode code label =
+  span []
+    [span [ bold ] [ text (label ++ ": ") ]
+    , text <| toString code
+    , text " / '"
+    , text <| String.fromList [ Char.fromCode code ]
+    , text "'"
+    ]
+  
 view : Model -> Html Msg
 view model =
   div []
     [ h2 [] [ text "Elm Keyboard Test" ]
-    , p [] [ span [ bold ] [ text "Code: " ]
-           , text <| toString model
-           , br
-           , span [ bold ] [ text "Char: " ]
-           , text <| String.fromList [ Char.fromCode model ]
+    , p [] [ displayCode model.press "press"
+           ,br
+           , displayCode model.down "down"
+           ,br
+           , displayCode model.up "up"
            ]
     , p [] [ text "Press keys and their keycodes will appear above." ]
-    , p [] [ text "Only the arrow keys work for me in Firefox." ]
-    , p [] [ text "The arrow keys do NOT work for me in Safari, Chrome, or Opera,"
-           , text " but the other keys DO work."
+    , p [] [ text "Only the arrow keys (and escape) get presses for for me in Firefox." ]
+    , p [] [ text "The arrow keys do NOT get presses in Safari, Chrome, or Opera,"
+           , text " but the other keys do."
            ]
     , p [] [ text "See "
            , a [ href "https://github.com/elm-lang/keyboard/issues/3" ]
